cc8a59f8e619debf3861c59825d2960bbdb99058
matejfric <matejfric@centrum.cz>
2024-04-01 11:08:13 +0200

    cleanup and antialiasing

--- dp.py
+++ dp.py
@@ -21,9 +21,16 @@ CY = HEIGHT // 3  # Y-axis mount point of the pendulum.
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-PURPLE = (128, 0, 128)
+
+BLUE = (31, 119, 180)
+ORANGE = (255, 127, 14)
+PURPLE = (148, 103, 189)
+RED = (214, 39, 40)
+GREEN = (44, 160, 44)
+
+COLOR1 = (33, 177, 255)
+COLOR2 = (255, 33, 140)
+COLOR3 = (33, 177, 255)
 # endregion
 
 
@@ -81,46 +88,25 @@ def double_pendulum_ode(
     return [V1, V2, a1, a2]
 
 
-def update_double_pendulum(p1: Pendulum, p2: Pendulum) -> tuple[Pendulum, Pendulum]:
-    # Constants
-    R1 = p1.r
-    R2 = p2.r
-    M1 = p1.m
-    M2 = p2.m
-    A1 = p1.a
-    A2 = p2.a
-    V1 = p1.v
-    V2 = p2.v
-    EPS = 1e-3
+def draw_thick_aaline(display, color, point0, point1, w):
+    # https://gist.github.com/gerryjenkinslb/8d433632ab541ad282f0c4fd49371b54
+    import math
 
-    # Acceleration
-    num1 = -G * (2 * M1 + M2) * np.sin(A1)
-    num2 = -M2 * G * np.sin(A1 - 2 * A2)
-    num3 = -2 * np.sin(A1 - A2) * M2
-    num4 = (V2**2) * R2 + (V1**2) * R1 * np.cos(A1 - A2)
-    den = R1 * (2 * M1 + M2 - M2 * np.cos(2 * A1 - 2 * A2))
-    a1 = (num1 + num2 + num3 * num4) / (den + EPS)
+    x0, y0 = point0
+    x1, y1 = point1
+    proportion = w / math.hypot(x1 - x0, y1 - y0) / 2
+    adjx = (x1 - x0) * proportion  # x side
+    adjy = (y1 - y0) * proportion  # y side
 
-    num1 = 2 * np.sin(A1 - A2)
-    num2 = ((V1**2) * R1 * (M1 + M2))
-    num3 = G * (M1 + M2) * np.cos(A1)
-    num4 = (V2**2) * R2 * M2 * np.cos(A1 - A2)
-    den = R2 * (2 * M1 + M2 - M2 * np.cos(2 * A1 - 2 * A2))
-    a2 = (num1 * (num2 + num3 + num4)) / (den + EPS)
+    pts = (
+        (x0 - adjy, y0 + adjx),  # A
+        (x0 + adjy, y0 - adjx),  # B
+        (x1 + adjy, y1 - adjx),  # C
+        (x1 - adjy, y1 + adjx),  # D
+    )
 
-    # Update velocity
-    p1.v += a1
-    p2.v += a2
-
-    # Update angle
-    p1.a += p1.v
-    p2.a += p2.v
-
-    # Damping of velocity
-    p1.v *= 0.995
-    p2.v *= 0.995
-
-    return p1, p2
+    pygame.draw.aalines(display, color, True, pts, True)  # outline
+    pygame.draw.polygon(display, color, pts, 0)  # fill it
 
 
 def draw_aa_filled_circle(surf, color, center, radius):
@@ -145,73 +131,35 @@ def draw_double_pendulum(
 
     # Draw trajectory of the second pendulum
     if prev_x2 and prev_y2:
-        pygame.draw.aaline(
-            trajectory_surface, BLUE, (int(prev_x2), int(prev_y2)), (int(x2), int(y2))
-        )
-        # pygame.gfxdraw.line(trajectory_surface, int(prev_x2), int(prev_y2), int(x2), int(y2), PURPLE)
+        draw_thick_aaline(trajectory_surface, COLOR3, (prev_x2, prev_y2), (x2, y2), 0.8)
 
-    screen.fill(WHITE)
+    screen.fill(BLACK)
     screen.blit(trajectory_surface, (0, 0))
 
-    # pygame.gfxdraw.line(screen, CX, CY, int(x1), int(y1), RED)
-    # pygame.gfxdraw.line(screen, int(x1), int(y1), int(x2), int(y2), BLUE)
-    pygame.draw.aaline(screen, RED, (CX, CY), (int(x1), int(y1)))
-    pygame.draw.aaline(screen, BLUE, (int(x1), int(y1)), (int(x2), int(y2)))
+    draw_thick_aaline(screen, COLOR1, (CX, CY), (int(x1), int(y1)), 3)
+    draw_thick_aaline(screen, COLOR2, (int(x1), int(y1)), (int(x2), int(y2)), 3)
 
-    draw_aa_filled_circle(screen, BLACK, (CX, CY), 4)
-    draw_aa_filled_circle(screen, RED, (int(x1), int(y1)), 8)
-    draw_aa_filled_circle(screen, BLUE, (int(x2), int(y2)), 8)
-    # pygame.gfxdraw.filled_circle(screen, CX, CY, 5, BLACK)
-    # pygame.gfxdraw.filled_circle(screen, int(x1), int(y1), 10, RED)
-    # pygame.gfxdraw.filled_circle(screen, int(x2), int(y2), 10, BLUE)
-    # pygame.draw.circle(screen, RED, (int(x1), int(y1)), 10)
-    # pygame.draw.circle(screen, BLUE, (int(x2), int(y2)), 10)
+    draw_aa_filled_circle(screen, COLOR1, (CX, CY), 4)
+    draw_aa_filled_circle(screen, COLOR1, (int(x1), int(y1)), 8)
+    draw_aa_filled_circle(screen, COLOR2, (int(x2), int(y2)), 8)
 
     pygame.display.flip()
 
     return x2, y2
 
 
-def main_euler():
-    """The numerical errors accumulate and the pendulum
-    quickly diverges from the expected behavior.
-    """
+def simulate_double_pendulum():
+    """Simulate the double pendulum with ODE45 numerical integration."""
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Double Pendulum Simulation")
     clock = pygame.time.Clock()
 
     # Pendulum parameters
-    p1 = Pendulum(r=100, m=20, a=PI / 3, v=0.00)
-    p2 = Pendulum(r=100, m=20, a=PI / 2, v=0.00)
+    p1 = Pendulum(r=125, m=20, a=PI / 3, v=0.00)
+    p2 = Pendulum(r=130, m=20, a=PI / 2, v=0.00)
 
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        # Update and draw pendulum
-        p1, p2 = update_double_pendulum(p1, p2)
-        draw_double_pendulum(screen, p1, p2)
-
-        clock.tick(60)
-
-    pygame.quit()
-
-
-def main_ode45():
-    """The ODE45 solver is more stable and the pendulum behaves as expected."""
-    pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Double Pendulum Simulation")
-    clock = pygame.time.Clock()
-
-    # Pendulum parameters
-    p1 = Pendulum(r=100, m=20, a=PI / 3, v=0.00)
-    p2 = Pendulum(r=100, m=20, a=PI / 2, v=0.00)
-
-    # Create a surface for drawing the trajectory
+    # Create a surface for drawing the trajectory of the second pendulum.
     trajectory_surface = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
 
     # Initial conditions
@@ -255,4 +203,4 @@ def main_ode45():
 
 
 if __name__ == "__main__":
-    main_ode45()
+    simulate_double_pendulum()
